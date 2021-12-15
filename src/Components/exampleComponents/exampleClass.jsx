217904c185514c4c4824a55fb61dd037e13dbe6f
DylanLovell <arcanyne7@gmail.com>
2021-12-15 11:13:38 -0500

    example files

---
+++ src/Components/exampleComponents/exampleClass.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Example.css';
+
+class Example extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className="Example"></div>;
+  }
+}
+
+export default Example;
