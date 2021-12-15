3d6977c828dec4c729c6575033bf1fb2b74f4bae
DylanLovell <arcanyne7@gmail.com>
2021-12-15 13:19:06 -0500

    nav-changes v1, hamburger v1

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,7 @@
+import './Logo.css';
+
+const Logo = () => {
+  return <div className="Logo"></div>;
+};
+
+export default Logo;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,24 @@
 import { Component } from 'react';
 import './Nav.css';
+import Logo from './Logo';
+import Hamburger from './Hamburger';
+import DropDown from './DropDown';
 
 class Nav extends Component {
   constructor() {
     super();
-    this.state = {};
   }
 
   render() {
-    return <div className="Nav">Nav</div>;
+    const { toggleDarkMode, toggleCustomCss, toggleDropMenu } = this.props;
+    const { darkMode, customCss, dropMenu } = this.props.state;
+    return (
+      <div className="Nav">
+        <Hamburger toggleDropMenu={toggleDropMenu} dropMenu={dropMenu} />
+        <Logo />
+        {dropMenu && <DropDown />}
+      </div>
+    );
   }
 }
 export default Nav;
