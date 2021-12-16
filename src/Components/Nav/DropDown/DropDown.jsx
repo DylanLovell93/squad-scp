d0a9eb4dd5b2ae1d8f6e938c9cb982f4d401dd86
DylanLovell <arcanyne7@gmail.com>
2021-12-15 22:33:58 -0500

    created default fellow, styled drop down

--- src/Components/Nav/DropDown/DropDown.jsx
+++ src/Components/Nav/DropDown/DropDown.jsx
@@ -2,8 +2,11 @@ import './DropDown.css';
 import fellowData from '../../../data/data';
 import ListItem from './ListItem/ListItem';
 
-const DropDown = () => {
-  const links = fellowData.map((fellow) => <ListItem fellow={fellow} />);
+const DropDown = (props) => {
+  const { toggleDropMenu } = props;
+  const links = fellowData.map((fellow) => (
+    <ListItem fellow={fellow} toggleDropMenu={toggleDropMenu} />
+  ));
   return (
     <div className="DropDown">
       <ul>{links}</ul>
